23860f0d87e49f3479b3a2c715a2f7c729ca9f92
Oleksii Bodnarchuk <olbody12@gmail.com>
2022-11-19 22:45:01 +0200

    added taxi-website.onrender.com to ALLOWED_HOSTS

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "taxi-service-django.herokuapp.com"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "taxi-service-django.herokuapp.com",
+    "taxi-website.onrender.com",
+]
 
 INTERNAL_IPS = [
     "127.0.0.1",
